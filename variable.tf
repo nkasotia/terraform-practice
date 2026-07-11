72e9a51b5ca13711d98734b237284e29bd041572
nkasotia <32234247+nkasotia@users.noreply.github.com>
2026-07-11 15:20:43 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,1 +1,16 @@
+variable "aws_region" {
+type = "string"
+}
 
+variable "vpc_name" {
+type = "string"
+}
+
+variable "vpc_cidr" {
+type = "string"
+}
+
+
+variable "bucket_name" {
+type = "string"
+}
