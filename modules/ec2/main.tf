f86ff3fc030cce0600229681b723a35d78ab90fe
nkasotia <32234247+nkasotia@users.noreply.github.com>
2026-07-12 09:59:33 +0530

    Update main.tf

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,1 +1,11 @@
+resource "ec2_instance" "nishant" {
+    ami = var.ami
+    instance_type = var.instance_type
+    subnet_id = var.subnet_id
+
+    tag {
+        name = var.instance_name
+    }
+  
+}
 
